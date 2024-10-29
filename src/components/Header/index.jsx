8343ca471980db5e6f2fdfd08d83aa5e2ac771d2
Muniba Rahman <muniba.rahman02@gmail.com>
2024-10-29 14:12:00 +0500

    Header modified

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,35 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from "./Header.module.css";
 import { Col, Container, Row } from 'react-bootstrap';
+import Drawer from 'react-modern-drawer';
+import 'react-modern-drawer/dist/index.css';
 import { HEADER_ICONS, HEADER_TAB } from '../../data/app-data';
+import { FaBars } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(prevState => !prevState);
+  };
+
   return (
+    <>
     <Container fluid>
-        <Row className={classes.rowClass}>
-            <Col md={4}>
-                <h1>Muniba Rahman</h1>
-            </Col>
-            <Col md={4}>
-                <div className={classes.tabDiv}>
-                    {HEADER_TAB?.map((e,i)=>{
-                        return(
-                            <div key={i}>
-                                <p className={classes.tab}>{e?.tab}</p>
-                            </div>
-                        )
-                    })}
-                </div>
-            </Col>
-            <Col md={4} className={classes.iconDiv}>
-                    {HEADER_ICONS?.map((e,i)=>{
-                        return(
-                            <a href={`${e?.path}`} target='_blank' className={classes.icon} key={i}>{e?.icon}</a>
-                          
-                        )
-                    })}
-            </Col>
-        </Row>
+      <Row className={classes.rowClass}>
+        <Col md={4}>
+          <h1 className={classes.title}>Muniba Rahman</h1>
+        </Col>
+        <Col md={4}>
+          <div className={classes.tabDiv}>
+            {HEADER_TAB?.map((e, i) => (
+              <div key={i}>
+                <p className={classes.tab} onClick={()=> navigate(`${e?.path}`)}>{e?.tab}</p>
+              </div>
+            ))}
+          </div>
+        </Col>
+        <Col md={4} className={classes.iconDiv}>
+          {HEADER_ICONS?.map((e, i) => (
+            <a href={`${e?.path}`} target='_blank' className={classes.icon} key={i} rel="noopener noreferrer">
+              {e?.icon}
+            </a>
+          ))}
+        </Col>
+        <span onClick={toggleDrawer} className={classes.drawerButton}>
+          <FaBars color='var(--white-color)' size={26} />
+          </span>
+      </Row>
+
+      <Drawer
+        open={isDrawerOpen}
+        onClose={toggleDrawer}
+        direction='right'
+        className={classes.drawer}
+      >
+        <div className={classes.drawerContent}>
+          {HEADER_TAB?.map((e, i) => (
+            <p key={i} className={classes.drawerTab} onClick={()=> navigate(`${e?.path}`)}>{e?.tab}</p>
+          ))}
+          <div className={classes.drawerIcons}>
+            {HEADER_ICONS?.map((e, i) => (
+              <a href={`${e?.path}`} target='_blank' className={classes.icon} key={i} rel="noopener noreferrer">
+                {e?.icon}
+              </a>
+            ))}
+          </div>
+        </div>
+      </Drawer>
     </Container>
-  )
+    </>
+  );
 }
